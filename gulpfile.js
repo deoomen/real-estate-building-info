3934523dc97a18f88c8949f118cea10948b0b7c2
deoomen <deoomen@protonmail.com>
2021-01-25 19:39:13 +0100

    add dev and prod commands

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const srcDir = 'src';
 const distDir = 'dist';
+const devDir = 'dev';
+let processDir = devDir;
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
@@ -8,12 +10,8 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const sourcemaps = require('gulp-sourcemaps');
-// const babel = require('gulp-babel');
+const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
-
-function destDir() {
-  return process.argv.indexOf('--prod') > -1 ? distDir : srcDir;
-}
 
 function scss() {
   return gulp
@@ -22,30 +20,53 @@ function scss() {
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([autoprefixer(), cssnano()]))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(destDir() + '/css'));
+    .pipe(gulp.dest(processDir + '/css'));
 }
 
 function js() {
   return gulp
     .src(srcDir + '/js/*.js')
-    // .pipe(babel({
-    //   presets: ['@babel/env']
-    // }))
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
     .pipe(uglify())
-    .pipe(gulp.dest(destDir() + '/js'));
+    .pipe(gulp.dest(processDir + '/js'));
 }
 
 function watch() {
+  processDir = devDir;
   gulp.watch(srcDir + '/scss/*.scss', scss);
-  // gulp.watch(srcDir + '/js/*.js', js);
 }
 
-function build() {
+function dev(done) {
+  processDir = devDir;
+  scss();
+  gulp
+    .src(srcDir + '/js/*.js')
+    .pipe(gulp.dest(processDir + '/js'));
+  gulp
+    .src(srcDir + '/index.html')
+    .pipe(gulp.dest(processDir));
+  gulp
+    .src(srcDir + '/sample.json')
+    .pipe(gulp.dest(processDir));
 
+  return done();
+}
+
+function prod(done) {
+  processDir = distDir;
+  scss();
+  js();
+  gulp
+    .src(srcDir + '/index.html')
+    .pipe(gulp.dest(processDir));
+
+  return done();
 }
 
 exports.watch = watch;
-exports.dev = scss;
-// exports.build = build;
+exports.dev = dev;
+exports.prod = prod;
 
-// exports.default = gulp.parallel(scss, js);
+exports.default = dev;
